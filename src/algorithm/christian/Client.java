194f0db97a4dcd08f68320ff04f2942f6ccfffdb
lucasemanuel8 <lucaxemanuel@gmail.com>
2018-10-26 15:27:50 -0300

    christian

--- src/algorithm/christian/Client.java
+++ src/algorithm/christian/Client.java
@@ -5,10 +5,14 @@
  */
 package algorithm.christian;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import javax.swing.JOptionPane;
+import java.util.Random;
 
 /**
  *
@@ -18,9 +22,24 @@ public class Client {
     public static void main(String[] args) { 
         try{
             Socket client = new Socket("localhost", 12345);
-            ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-            Date currentDate = (Date)in.readObject();
-            System.out.println("Date of server: " + currentDate.toString());
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            String msg = "Qual é o tempo?";
+            
+            long t1 = System.currentTimeMillis();
+            out.writeUTF(msg);
+//            long t2 = in.readInt();
+            long serverTime = in.readLong();
+//            long t3 = serverTime;
+            Random rand= new Random();
+            Thread.sleep(rand.nextInt(10000));
+            long t4 = System.currentTimeMillis();
+            
+            long time = serverTime + (t4 - t1)/2;
+            DateFormat simDate = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS");
+            Date result = new Date(time); 
+            System.out.println(new Date(t1) + "\n" + new Date(t4) + "\n" + simDate.format(result) + "\n");
+//            System.out.println("Date of server: " + currentDate.toString());
             in.close();
             System.out.println("Connect finally!");
         }catch(Exception e) {
--- src/algorithm/christian/Server.java
+++ src/algorithm/christian/Server.java
@@ -5,11 +5,12 @@
  */
 package algorithm.christian;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.Random;
 
 /**
  *
@@ -20,12 +21,20 @@ public class Server {
         try {
             ServerSocket server = new ServerSocket(12345);
             System.out.println("Server Lister a port 12345");
+
             while(true){
                 Socket client = server.accept();    
-                System.out.println("Addr client: " + client.getInetAddress().getHostAddress());
-                ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+//                System.out.println("Addr client: " + client.getInetAddress().getHostAddress());
+                DataInputStream in = new DataInputStream(client.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                out.writeLong(System.currentTimeMillis());
+                Random rand= new Random();
+                Thread.sleep(rand.nextInt(1000));
+//                DataInputStream dis = new DataInputStream(client.getInputStream()); 
+//                String aff = dis.readUTF();
                 out.flush();
-                out.writeObject(new Date());
+                out.writeInt((int)System.currentTimeMillis());
                 out.close();
                 client.close();
             }
